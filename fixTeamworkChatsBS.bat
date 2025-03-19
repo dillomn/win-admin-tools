b98df5578fdcf38428a16cff214d7a7d92a13c3b
Dillon <66445324+dillomn@users.noreply.github.com>
2025-03-19 13:42:02 +1100

    Update fixTeamworkChatsBS.bat
    Now kills the task to make sure it isn't running which prevents it form clearing the bad cache files, then re-opens automatically.

--- fixTeamworkChatsBS.bat
+++ fixTeamworkChatsBS.bat
@@ -1,5 +1,12 @@
 @echo off
 set "folder=C:\Users\%username%\AppData\Roaming\TeamworkChat\Cache"
+set "TeamworkChat=C:\Users\%username%\AppData\Local\TeamworkChat\TeamworkChat.exe"
+
+:: Kill's Teamwork Chat process so that the cache can be deleted.
+taskkill /IM TeamworkChat.exe /F
+
+:: Wait for TeamworkChat to stop before clearing the cache
+timeout /t 2 /nobreak >nul
 
 pushd "%folder%"
 for %%F in (*) do (
@@ -8,3 +15,9 @@ for %%F in (*) do (
     )
 )
 popd
+
+:: Wait for TeamworkChat cache to be cleared
+timeout /t 2 /nobreak >nul
+
+:: Re-open TeamworkChat
+start "" "%TeamworkChat%"
